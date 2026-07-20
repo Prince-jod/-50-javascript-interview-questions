4d8fb5a65c5779ab1b866f54f624d83d7d5343bf
Prince-jod <baghelprince164@gmail.com>
2026-07-20 23:41:59 +0530

    Auto commit

---
+++ que!.js
@@ -0,0 +1,8 @@
+// explain the concept of hoisting in javascript 
+console.log(a); //undefined this is called hoisting in javascript. Hoisting is a behavior in JavaScript where variable and function declarations are moved to the top of their containing scope during the compilation phase. This means that you can use variables and functions before they are actually declared in the code.
+var a=10;
+console.log(a); //10
+
+console.log(b);
+
+let b=20;
