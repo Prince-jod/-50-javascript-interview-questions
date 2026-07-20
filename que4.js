346b3ea7400cbc186b30467e8be9c07a0de1b8fc
Prince-jod <baghelprince164@gmail.com>
2026-07-20 23:58:42 +0530

    Auto commit

---
+++ que4.js
@@ -0,0 +1,8 @@
+//explain the diff between null and undefined in javascript 
+//a varibale is declared but not assigned a value then it is undefined. it mean that the variable has been declared but has not yet been assigned a value.
+var a;
+console.log(a); //undefined
+
+//null is an assignment value. it can be assigned to a variable as a representation of no value. it means that the variable has been explicitly assigned a value of null, indicating that it has no value or object.
+var b=null;
+console.log(b); //null
