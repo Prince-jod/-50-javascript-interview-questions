243987d555dbbf165a573e8f4e0554cd6851d994
Prince-jod <baghelprince164@gmail.com>
2026-07-24 01:39:24 +0530

    Auto commit

--- map.js
+++ map.js
@@ -1,8 +1,9 @@
 //using map on array and def of parameter passed in an array 
 
-const arr=[10,20,30];
-arr.map((value,index,arr)=>{
- console.log(value);
- console.log(index);
- console.log(arr);
-})
+const arr = [10, 20, 30];
+
+arr.map((value, index, array) => {
+    console.log(value);
+    console.log(index);
+    console.log(array);
+});
