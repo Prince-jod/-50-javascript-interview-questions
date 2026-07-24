adc521f7f9b6c290f8847ef6b2659a833a657058
Prince-jod <baghelprince164@gmail.com>
2026-07-24 23:52:58 +0530

    Auto commit

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const http=require('http');
 const port=3000;
 const server=http.createServer((req,res)=>{
  console.log("server is created");
- if(req.url==='/'){
+ res.setHeaders('Content-type','text/html');
+ if(req.url=='/'){
+  res.statusCode=200;//ok
   res.end("Home page");
+ }
+ else{
+  if(req.url=='/task'){
+    res.statusCode=200;//ok
+    res.end("this is the task page ")
+  }
+  else{
+    res.statusCode=404;//ok
+    res.end("not found the page");
+  }
  }
 })
 server.listen(port,()=>{
