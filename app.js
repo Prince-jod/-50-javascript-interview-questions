24884a2470aa1fa7dc3a9995a26b89cc24e63aa6
Prince-jod <baghelprince164@gmail.com>
2026-07-24 23:53:56 +0530

    Auto commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http=require('http');
 const port=3000;
 const server=http.createServer((req,res)=>{
  console.log("server is created");
- res.setHeaders('Content-type','text/html');
+ res.setHeader('Content-type','text/html');
  if(req.url=='/'){
   res.statusCode=200;//ok
   res.end("Home page");
