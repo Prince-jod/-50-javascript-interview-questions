ebe3dc86c4ead9870cf8f75d5795bfb0d80f6a4f
Prince-jod <baghelprince164@gmail.com>
2026-07-25 00:38:55 +0530

    Auto commit

--- FileReadingAndWriting.js
+++ FileReadingAndWriting.js
@@ -1,1 +1,18 @@
 //node js for file reading and writing 
+//creating a from 
+<form action="/submit" method="POST"></form> //when the user will click at the submit button the action will see that and will route the /submita
+//and method will tell the browser how to send the data 
+    <label>Name:</label>
+    <input type="text" name="username">
+
+    <br><br>
+
+    <button type="submit">Submit</button>
+</form>
+ creating a buffer
+const buffer = Buffer.from("Prince");
+
+console.log(buffer);
+buffer stores raw binary data for temp
+because computer dont understand string they understand byte 
+output---{/* <Buffer 50 72 69 6e 63 65> */}
