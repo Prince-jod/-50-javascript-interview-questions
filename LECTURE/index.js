89d44c577cc144ad9e13a2d837f0427c019b3ccb
Prince-jod <baghelprince164@gmail.com>
2026-07-26 00:25:17 +0530

    Auto commit

--- LECTURE/index.js
+++ LECTURE/index.js
@@ -1,3 +1,7 @@
 const http=require('http');
 const routes=require('./routes');
 const server=http.createServer(routes.handler);
+const port=3000;
+server.listen(port,()=>{
+  console.log(`server is running on port number ${port}`);
+});
