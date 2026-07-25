186eb786085ad59edc7a32449ac61ceff00861dc
Prince-jod <baghelprince164@gmail.com>
2026-07-25 23:48:41 +0530

    Auto commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const port = 3000;
 const routes=require('/.routes');
-const server = http.createServer();
+const server = http.createServer(routes);
 
 server.listen(port, () => {
   console.log(`server is listing at ${port}`);
