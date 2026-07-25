9e5253df47199ae66be2ed51c962b9525ad73fa0
Prince-jod <baghelprince164@gmail.com>
2026-07-26 03:27:04 +0530

    Auto commit

---
+++ Express/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const port = 4000;
+app.use((req,res,next)=>{
+  req.user="guest";
+  next();
+})
+app.get("/", (req, res) => {
+    res.set("Content-Type", "text/html");
+    res.send(`<h1>${req.user}</h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}! Ready to handle requests.`);
+});
