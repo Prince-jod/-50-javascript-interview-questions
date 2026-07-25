a1e203fbfef6337ab6f9959a8a94907bbc013180
Prince-jod <baghelprince164@gmail.com>
2026-07-25 23:49:30 +0530

    Auto commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const port = 3000;
-const routes=require('/.routes');
+const routes=require('./routes');
 const server = http.createServer(routes);
 
 server.listen(port, () => {
