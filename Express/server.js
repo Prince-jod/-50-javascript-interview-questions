207dc6fd2cb2d0df53fd89ca8440658bd74f8319
Prince-jod <baghelprince164@gmail.com>
2026-07-26 02:45:24 +0530

    Auto commit

--- Express/server.js
+++ Express/server.js
@@ -1,6 +1,8 @@
-const express=require("express");
-const server=express();
-const port=8000;
-server.listen(port,()=>{
-  console.log("Server is up and running on port 3000! Ready to handle requests.")
+const express = require("express");
+
+const server = express();
+const port = 8000;
+
+server.listen(port, () => {
+  console.log(`Server is up and running on port ${port}! Ready to handle requests.`);
 });
