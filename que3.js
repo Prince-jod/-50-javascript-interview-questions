975f270c337c4c1524b59f197a7fb595c9b109eb
Prince-jod <baghelprince164@gmail.com>
2026-07-26 02:10:20 +0530

    Auto commit

---
+++ que3.js
@@ -0,0 +1,9 @@
+//what are primitives in javascript
+//primitives are the most basic data types in javascript. They are immutable and are not objects. The primitive data types in javascript are:
+//1. string 
+//2.number
+//3.boolean
+//4.null
+//5.undefined
+//6.symbol (ES6)
+//7.bigint (ES11)
