a96c60105323fdac55cad64c36f598c62493c3e9
Prince-jod <baghelprince164@gmail.com>
2026-07-26 17:16:28 +0530

    Auto commit

--- Express/Serve.js
+++ Express/Serve.js
@@ -5,10 +5,10 @@ const PORT = 3000;
 
 // GET /products
 app.get("/products/:name", (req, res) => {
-    res.send({
-      params:req.params,
-      query:req.query
-});
+  const params=req.params.name;
+  const name=req.query.name;
+  const age=req.query.age;
+    res.send(`welcome ${params} and your name is ${name} and your age is ${age}`);
 });
 
 // POST /products
