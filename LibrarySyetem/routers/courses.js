27d69929466b6309b2cc8bc2f331fa53f6eec83a
Prince-jod <baghelprince164@gmail.com>
2026-07-27 03:40:57 +0530

    Auto commit

--- LibrarySyetem/routers/courses.js
+++ LibrarySyetem/routers/courses.js
@@ -1,16 +1,28 @@
 const express = require("express");
 
 const router = express.Router();
+const courses = [
 
+{ id: 1, name: "Frontend", description: "HTML, CSS, JS, React" },
+
+{ id: 2, name: "Backend", description: "Node.js, Express, MongoDB" }
+
+];
 // GET /courses
 router.get("/", (req, res) => {
-    res.send("List of all courses");
+    res.json(courses);
 });
 
 // GET /courses/:id
 router.get("/:id", (req, res) => {
-    const id = req.params.id;
-    res.send(`Course ID: ${id}`);
+    const id=parseInt(req.params.id);
+    const course=courses.find((x)=x.id==id);
+    if(!course){
+      return ({
+        message:"course not found",
+      })
+    }
+    res.json(course);
 });
 
 module.exports = router;
