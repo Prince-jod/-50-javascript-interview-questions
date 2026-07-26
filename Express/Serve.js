a967408c239ada542f7b45b5d0818736b1cfb994
Prince-jod <baghelprince164@gmail.com>
2026-07-26 15:19:19 +0530

    Auto commit

---
+++ Express/Serve.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// GET /products
+app.get("/products", (req, res) => {
+    res.send("Here is the list of all products.");
+});
+
+// POST /products
+app.post("/products", (req, res) => {
+    res.send("A new product has been added.");
+});
+
+// GET /categories
+app.get("/categories", (req, res) => {
+    res.send("Here is the list of all categories.");
+});
+
+// POST /categories
+app.post("/categories", (req, res) => {
+    res.send("A new category has been created.");
+});
+
+// Wildcard route (must be the last route)
+app.all("*", (req, res) => {
+    res.status(404).send("<h1>404 - Page Not Found</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
