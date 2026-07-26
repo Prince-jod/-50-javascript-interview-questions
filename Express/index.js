a85e2d44b67acc5d5218a389d6f862e6aa9097ea
Prince-jod <baghelprince164@gmail.com>
2026-07-27 01:22:28 +0530

    Auto commit

--- Express/index.js
+++ Express/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
-const orderRouter=require('./Router/Order');
+const orderRouter=require('./Routers/Order');
 // GET /orders
 app.get('/',(req,res)=>{
     res.send("welocme to the server first page");
