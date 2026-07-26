b023fe359a1f74df14a1531840c66fb0efee1611
Prince-jod <baghelprince164@gmail.com>
2026-07-27 03:16:17 +0530

    Auto commit

--- LibrarySyetem/routers/Book.js
+++ LibrarySyetem/routers/Book.js
@@ -1,13 +1,13 @@
 const express=require('express');
 const router=express.Router();
 
-router.get('/books',(req,res)=>{
+router.get('/',(req,res)=>{
   req.setHeader('Content-type','text/html');
   res.send(`
   <h1>    Here is the list of books! </h1>
     `)
 });
-router.post('/books',(req,res)=>{
+router.post('/',(req,res)=>{
   res.send("Book has been added!");
 })
 module.exports=router;
