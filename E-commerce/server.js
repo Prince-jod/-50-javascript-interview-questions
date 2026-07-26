133d6f536c9e6453654bc49ba48127efcb0b8680
Prince-jod <baghelprince164@gmail.com>
2026-07-27 03:54:04 +0530

    Auto commit

---
+++ E-commerce/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+app.use("/cart", cartRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
