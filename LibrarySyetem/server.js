e37863df9e3322469ca30db208c3080859975a98
Prince-jod <baghelprince164@gmail.com>
2026-07-27 03:25:16 +0530

    Auto commit

--- LibrarySyetem/server.js
+++ LibrarySyetem/server.js
@@ -1,14 +1,25 @@
-const express=require('express');
-const app=express();
-const PORT=3000;
+const express = require("express");
 
-const  bookRouter=require('./routers/Book');
+const app = express();
+const PORT = 4000;
 
-app.use('/books',bookRouter);
+const studentRouter = require("./routes/students");
+const courseRouter = require("./routes/courses");
 
+// Home Route
+app.get("/", (req, res) => {
+    res.send("Welcome to the Student & Course API");
+});
 
+// Routes
+app.use("/students", studentRouter);
+app.use("/courses", courseRouter);
 
+// 404 Handler
+app.use((req, res) => {
+    res.status(404).send("Page not found");
+});
 
-app.listen(PORT,()=>{
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
