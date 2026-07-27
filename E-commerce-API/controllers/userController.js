7b162c01972d0e9a7eeccb23e2c9e470434b83f9
Prince-jod <baghelprince164@gmail.com>
2026-07-27 23:39:26 +0530

    Auto commit

--- E-commerce-API/controllers/userController.js
+++ E-commerce-API/controllers/userController.js
@@ -1,16 +1,12 @@
-const getAllusers=(req,res)=>{
-  res.send("Fetching all users");
-}
-const addAllProduct=(req,res)=>{
-  res.send("Adding new Product");
-}
-const getProductById=(req,res)=>
-{
-  const id=req.params.id;
-   res.send(`Fetching product with ID: ${id}`);
-}
-module.exports={
-  getAllusers,
-  addAllProduct,
-  getProductById,
+exports.getAllUsers = (req, res) => {
+    res.send("Fetching all users");
+};
+
+exports.addUser = (req, res) => {
+    res.send("Adding a new user");
+};
+
+exports.getUserById = (req, res) => {
+    const id = req.params.id;
+    res.send(`Fetching user with ID: ${id}`);
 };
