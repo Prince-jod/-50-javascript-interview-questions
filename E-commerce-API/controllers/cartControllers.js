bc9cef7726331c63a26d7136509ee70285088c2b
Prince-jod <baghelprince164@gmail.com>
2026-07-27 23:39:22 +0530

    Auto commit

--- E-commerce-API/controllers/cartControllers.js
+++ E-commerce-API/controllers/cartControllers.js
@@ -1,13 +1,9 @@
-const getCartForUser = (req, res) => {
+exports.getCartForUser = (req, res) => {
     const userId = req.params.userId;
     res.send(`Fetching cart for user with ID: ${userId}`);
 };
 
-const addProductToCart = (req, res) => {
+exports.addProductToCart = (req, res) => {
     const userId = req.params.userId;
     res.send(`Adding product to cart for user with ID: ${userId}`);
 };
-module.exports={
-  getCartForUser,
-  addProductToCart,
-}
