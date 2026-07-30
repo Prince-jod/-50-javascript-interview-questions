07a6ed946fc5eec04135e3eac6714db0f503af69
Prince-jod <baghelprince164@gmail.com>
2026-07-31 00:26:38 +0530

    Auto commit

---
+++ TestDB/index.js
@@ -0,0 +1,9 @@
+const express =require('express');
+const app=express();
+const port=6000;
+app.get('/',(req,res)=>{
+  res.send("shame on youhh mf!!")
+})
+app.listen(port,(err)=>{
+  console.log("server is running on ",port);
+});
