f41a485b827f39ab90d91b3065aaa10d84952c80
Prince-jod <baghelprince164@gmail.com>
2026-07-31 02:31:38 +0530

    Auto commit

---
+++ TestDB/Routers/StudentRoutes.js
@@ -0,0 +1,7 @@
+const express =require ('express');
+const studentController=require('./Controllers/StudentController');
+const router=express.Router();
+
+router.post('/',)
+
+module.exports=router;
