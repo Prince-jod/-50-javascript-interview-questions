7fb8ee63c8400a7b49a74acc68ef44bcee6f0b37
Prince-jod <baghelprince164@gmail.com>
2026-07-31 00:30:39 +0530

    Auto commit

--- TestDB/index.js
+++ TestDB/index.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const app=express();
-const mysql=require('mysql');
+const mysql=require('mysql2');
 const port=4000;
 app.get('/',(req,res)=>{
   res.send("shame on youhh mf!!")
