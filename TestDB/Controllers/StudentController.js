ad8ba55ff0e3fa5b06a49d600e6e8b2c8694509d
Prince-jod <baghelprince164@gmail.com>
2026-07-31 11:36:18 +0530

    Auto commit

--- TestDB/Controllers/StudentController.js
+++ TestDB/Controllers/StudentController.js
@@ -1,9 +1,12 @@
-const db=require('mysql2');
+const db=require('../utils/db-connection');
 
 const addEntries=(req,res)=>{
 const [email,name]=req.body; // taking data my body
 const insertquery=`INSERT INTO (email,name) VALUE (?,?)`;
-
+db.execute(insertquery,[email,name],(err)=>{
+  console.log(err);
+  connection.end
+})
 }
 module.exports={
   addEntries,
