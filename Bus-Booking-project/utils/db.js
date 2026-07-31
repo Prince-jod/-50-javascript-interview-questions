f6a27397a59f0f97fa010feedb2b0b44a1bb4f41
Prince-jod <baghelprince164@gmail.com>
2026-08-01 01:37:44 +0530

    Auto commit

--- Bus-Booking-project/utils/db.js
+++ Bus-Booking-project/utils/db.js
@@ -1,1 +1,11 @@
 const {Sequelize}=require('sequelize');
+
+const sequelize=new Sequelize('testdb','root','Prince007@',{
+  user:"root",
+  dialect:'mysql'
+});
+sequelize.authenticate()
+    .then(() => console.log("Database Connected"))
+    .catch(err => console.log(err));
+
+module.exports = sequelize;
