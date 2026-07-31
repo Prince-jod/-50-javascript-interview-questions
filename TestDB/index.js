67b43f9869855ad0561d1a171838466a12df50a0
Prince-jod <baghelprince164@gmail.com>
2026-08-01 00:21:30 +0530

    Auto commit

--- TestDB/index.js
+++ TestDB/index.js
@@ -9,12 +9,11 @@ app.use(express.json());
 app.use("/students", studentRoutes);
 
 db.sync().then((res)=>{
-console.log(res);
-}).catch((err)=>{
-    console.log(err);
-})
 app.listen(4000, () => {
 
     console.log("Server Running On Port 4000");
 
 });
+}).catch((err)=>{
+    console.log(err);
+})
