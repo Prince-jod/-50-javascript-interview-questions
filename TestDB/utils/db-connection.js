22bcb9eb2b9ee578178648db836e8d5042ce8c35
Prince-jod <baghelprince164@gmail.com>
2026-08-01 00:08:09 +0530

    Auto commit

--- TestDB/utils/db-connection.js
+++ TestDB/utils/db-connection.js
@@ -1,19 +1,78 @@
-const mysql = require("mysql2");
+const {Sequelize} =require('Sequelize');
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Prince007@",
-    database: "testdb"
+const sequelize=new Sequelize('testdb','root','Prince007@',{
+    host:'localhost',
+    dialect:'Mysql'
 });
 
-db.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
+(
+    async ()=>{
+        try{
+            sequelize.authenticate();
+            console.log("data connetion has een made");
+        }
+        catch(err){
+   console.log(err);
+        }
     }
+)
+();
+model.exports=sequelize;
 
-    console.log("Database Connected");
-});
 
-module.exports = db;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mysql = require("mysql2");
+
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "Prince007@",
+//     database: "testdb"
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+
+//     console.log("Database Connected");
+// });
+
+// module.exports = db;
