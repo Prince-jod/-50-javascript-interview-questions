a9918a5084db090b20f967907ef7f125538382a4
Prince-jod <baghelprince164@gmail.com>
2026-07-31 12:51:39 +0530

    Auto commit

--- TestDB/Controllers/StudentController.js
+++ TestDB/Controllers/StudentController.js
@@ -1,23 +1,19 @@
-const addEntries = (req, res) => {
-    console.log("1. Controller reached");
+const db=require('../utils/db-connection');
 
-    const { id, name, dept_id } = req.body;
+const addEntries=(req,res)=>{
+const {id,name,dept_id}=req.body; // taking data my body
+const insertquery=`INSERT INTO employees VALUE (?,?,?)`;
+db.execute(insertquery,[id,name,dept_id],(err)=>{
+if (err) {
+    console.log(err);
+    return res.status(500).json({ error: err.message });
+}
+ res.status(201).json({
+    message: "Inserted successfully"
+});
 
-    const insertQuery = "INSERT INTO employees VALUES (?, ?, ?)";
-
-    db.execute(insertQuery, [id, name, dept_id], (err, result) => {
-        console.log("2. Query callback reached");
-
-        if (err) {
-            console.log("3. Query failed:", err);
-            return res.status(500).json({ error: err.message });
-        }
-
-        console.log("4. Query executed successfully");
-        console.log(result);
-
-        res.status(201).json({
-            message: "Inserted successfully"
-        });
-    });
-};
+})
+}
+module.exports={
+  addEntries,
+} 
