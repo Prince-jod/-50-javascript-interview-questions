c4dbbb177c2cd06ea134d022765b349008f1367d
Prince-jod <baghelprince164@gmail.com>
2026-07-31 12:11:09 +0530

    Auto commit

--- TestDB/Controllers/StudentController.js
+++ TestDB/Controllers/StudentController.js
@@ -1,11 +1,10 @@
 const db=require('../utils/db-connection');
 
 const addEntries=(req,res)=>{
-const {email,name}=req.body; // taking data my body
-const insertquery=`INSERT INTO (email,name) VALUE (?,?)`;
-db.execute(insertquery,[email,name],(err)=>{
+const {id,name,dept_id}=req.body; // taking data my body
+const insertquery=`INSERT INTO buses VALUE (?,?,?)`;
+db.execute(insertquery,[id,name,dept_id],(err)=>{
   if(err){
-  console.log(err);
   connection.end();
   return;
   }
