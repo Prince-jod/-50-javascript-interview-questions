92aab6c821bb433d152bb1ede6ab4fd82a0d0845
Prince-jod <baghelprince164@gmail.com>
2026-07-31 16:39:04 +0530

    Auto commit

--- TestDB/Routers/StudentRoutes.js
+++ TestDB/Routers/StudentRoutes.js
@@ -1,8 +1,17 @@
-const express =require ('express');
-const studentController=require('../Controllers/StudentController');
-const router=express.Router();
+const express = require("express");
 
-router.post('/add',studentController.addEntries);
-router.put('/update/:id',studentController.updateEntry);
+const router = express.Router();
 
-module.exports=router;
+const studentController = require("../Controllers/StudentController");
+
+router.post("/", studentController.addStudent);
+
+router.get("/", studentController.getStudents);
+
+router.get("/:id", studentController.getStudentById);
+
+router.put("/:id", studentController.updateStudent);
+
+router.delete("/:id", studentController.deleteStudent);
+
+module.exports = router;
