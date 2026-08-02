e1f9a2bb389ada0fe99a39228b3100d913d6553c
Prince-jod <baghelprince164@gmail.com>
2026-08-03 02:19:39 +0530

    Auto commit

--- Booking-pp/Models/association.js
+++ Booking-pp/Models/association.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const Aadhaar = require("../models/Aadhaar");
+const Aadhaar = require("../models/Aadhaarid");
 
 // One User has One Aadhaar
 User.hasOne(Aadhaar, {
