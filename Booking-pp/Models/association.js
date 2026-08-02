721c90ffd80c22e4ef771bd7a4154bfab2b7d4f2
Prince-jod <baghelprince164@gmail.com>
2026-08-03 02:19:21 +0530

    Auto commit

---
+++ Booking-pp/Models/association.js
@@ -0,0 +1,21 @@
+const User = require("../models/User");
+const Aadhaar = require("../models/Aadhaar");
+
+// One User has One Aadhaar
+User.hasOne(Aadhaar, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+// One Aadhaar belongs to One User
+Aadhaar.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+module.exports = {
+  User,
+  Aadhaar,
+};
