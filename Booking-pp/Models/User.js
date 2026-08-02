25ed2683dd07088f56dc9db021d0e75ceefd48dc
Prince-jod <baghelprince164@gmail.com>
2026-08-03 00:21:51 +0530

    Auto commit

--- Booking-pp/Models/User.js
+++ Booking-pp/Models/User.js
@@ -1,3 +1,22 @@
 const {DataType} =require('sequelize');
 
-const sequelize=require('./config/db');
+const sequelize=require('../config/db');
+
+const User=sequelize.define("User", {
+  name:{
+    type:DayaTypes.STRING,
+    allowNull:false
+  },
+  email:{
+    type:DataTypes.STRING,
+    allowNull:false,
+    unique:true
+  },
+  phone:{
+   type:DataTypes.INTEGER,
+   allowNull:true,
+   unique:true
+  }
+})
+
+module.exports=User
