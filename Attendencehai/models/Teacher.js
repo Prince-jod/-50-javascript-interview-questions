79d7fe44e5fe5e412a40eda90a6192d472a40ecb
Prince-jod <baghelprince164@gmail.com>
2026-08-06 00:19:49 +0530

    Auto commit

--- Attendencehai/models/Teacher.js
+++ Attendencehai/models/Teacher.js
@@ -1,2 +1,44 @@
 const {DataTypes}=require('sequelize');
 const sequelize=require('../config/db');
+
+
+const Teacher=sequelize.define("Teacher",{
+  id:{
+    type:DataTypes.INTEGER,
+    primaryKey:true,
+    autoIncrement:true,
+
+  },
+  name:{
+   type:DataTypes.STRING(200),
+   allowNull:false,
+   validate:{
+    notEmpty:true,
+   }
+  },
+  email:{
+    type:DataTypes.STRING(300),
+    allowNull:false,
+    unique:true,
+    vlaidate:{
+      idEmail:true,
+      notEmpty:true,
+    }
+  },
+  password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    
+},
+ {
+    tableName: "teachers",
+    timestamps: true,
+  }
+
+);
+
+module.exports=Teacher;
