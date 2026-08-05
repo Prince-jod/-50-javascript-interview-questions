741d1fa58983b6260d07c9f0508074657b4149e3
Prince-jod <baghelprince164@gmail.com>
2026-08-06 03:08:19 +0530

    Auto commit

--- Attendencehai/controllers/auth.controller.js
+++ Attendencehai/controllers/auth.controller.js
@@ -58,3 +58,44 @@ const register = async (req, res) => {
 module.exports = {
   register,
 };
+
+const login= async (req,res)=>{
+  try{
+    const {email,password}=req.body;
+
+    if(!email||!password){
+      return res.status(400).json({
+        message:"email and password is required"
+      })
+
+    }
+    const teacher=await Teacher.findOne({
+      where:{
+        email,
+      }
+    })
+    if(!teacher){
+      return res.status(401).json({
+        message:"invalid name and password"
+      })
+    }
+    const isMatch = await bcrypt.compare(password, teacher.password);
+    if(!isMatch){
+      return res.status(401).json({
+        message:"invalid password"
+      })
+    }
+    return res.status(200).json({
+      message:"login successfully"
+  });
+  
+  
+}
+catch (error) {
+    console.error("Login Error:", error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
