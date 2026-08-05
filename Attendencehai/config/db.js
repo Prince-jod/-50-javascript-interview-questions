76098e2697cdbaf0ce4a2cb3dec2b654d62bb28f
Prince-jod <baghelprince164@gmail.com>
2026-08-05 17:36:18 +0530

    Auto commit

---
+++ Attendencehai/config/db.js
@@ -0,0 +1,18 @@
+//first we will import sequelize 
+
+const {Sequelize}=require('sequelize');
+
+require('dotevn').config();
+
+const sequelize=new Sequelize(
+
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env,DB_PASSWORD,
+  {
+    host:process.env.DB_HOST,
+    dialect:process.env.DB_DIALECT,
+  }
+);
+
+module.exports=sequelize;
