c4c29888f0191febf35c2f028e5a958087ee95c3
Prince-jod <baghelprince164@gmail.com>
2026-08-05 17:43:35 +0530

    Auto commit

---
+++ Attendencehai/server.js
@@ -0,0 +1,13 @@
+const express =require('express');
+const cors=require('cors');
+require('dotenv').config();
+
+const sequelize=require('../config/db');
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+sequelize.authenticate()
+
