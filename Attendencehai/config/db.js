1501f6dd8cbeb53a8a21c799b4f440637685c3de
Prince-jod <baghelprince164@gmail.com>
2026-08-05 17:50:33 +0530

    Auto commit

--- Attendencehai/config/db.js
+++ Attendencehai/config/db.js
@@ -2,7 +2,7 @@
 
 const {Sequelize}=require('sequelize');  /// imported it so we can make the connection to the database
 
-require('dotevn').config(); // this will help us to read the env file which contains info about database connection
+require('dotenv').config(); // this will help us to read the env file which contains info about database connection
 
 const sequelize=new Sequelize( //here we are telling to the sequelize that which database connection it needs to be connect providing the cred to te sequelize constructr
   //where it takes the db name ad user and password as paraenter and also a object that contains the host info and dialect info
