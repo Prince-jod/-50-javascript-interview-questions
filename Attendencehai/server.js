726822fa30ab8b026c6f057ab8e1655f14eb2fc0
Prince-jod <baghelprince164@gmail.com>
2026-08-06 03:14:13 +0530

    Auto commit

--- Attendencehai/server.js
+++ Attendencehai/server.js
@@ -5,10 +5,12 @@ require('dotenv').config();
 const sequelize=require('./config/db');
 
 const app=express();
-
+const path=require('path');
 app.use(cors());
 app.use(express.json());
 
+
+app.use(express.static(path.join(__dirname, "public")));
 sequelize.authenticate()
 .then(()=>console.log("database connected  successfully"))
 .catch((err)=>console.log("got an error",err));
