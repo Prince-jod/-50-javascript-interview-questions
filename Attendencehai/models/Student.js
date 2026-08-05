031fc2f3268cfcaae630800dd8bb7698468da265
Prince-jod <baghelprince164@gmail.com>
2026-08-06 00:34:53 +0530

    Auto commit

---
+++ Attendencehai/models/Student.js
@@ -0,0 +1,58 @@
+const {DataTypes}=require('sequelize');
+
+
+const sequelize=require('../config/db');
+
+const Student=sequelize.define("Student",{
+  id:{
+    type:DataTypes.INTEGER,
+    autoIncrement:true,
+    primaryKey:true,
+    validate:{
+      notEmpty:true
+    }
+  },
+  name:{
+    type:DataTypes.STRING(30),
+    allowNull:false,
+    validate:{
+      notEmpty:true
+    }
+  },
+  rollNumber:{
+    type:DataTypes.SRTING(200),
+    allowNull:false,
+    unique:true,
+    validate:{
+      isEmpty:false
+    }
+  },
+  section: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+      
+    email: {
+      type: DataTypes.STRING(100),
+      unique: true,
+      allowNull: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+     phone: {
+      type: DataTypes.STRING(15),
+      allowNull: true,
+    },
+    
+
+},
+  {
+    tableName: "students",
+    timestamps: true,
+  }
+);
+module.exports=Student;
