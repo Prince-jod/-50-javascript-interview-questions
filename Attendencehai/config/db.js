ca56ae0b7cc6b19c41c2f963cbae698cf7ae35e1
Prince-jod <baghelprince164@gmail.com>
2026-08-05 17:48:56 +0530

    Auto commit

--- Attendencehai/config/db.js
+++ Attendencehai/config/db.js
@@ -1,10 +1,11 @@
 //first we will import sequelize 
 
-const {Sequelize}=require('sequelize');
+const {Sequelize}=require('sequelize');  /// imported it so we can make the connection to the database
 
-require('dotevn').config();
+require('dotevn').config(); // this will help us to read the env file which contains info about database connection
 
-const sequelize=new Sequelize(
+const sequelize=new Sequelize( //here we are telling to the sequelize that which database connection it needs to be connect providing the cred to te sequelize constructr
+  //where it takes the db name ad user and password as paraenter and also a object that contains the host info and dialect info
 
   process.env.DB_NAME,
   process.env.DB_USER,
