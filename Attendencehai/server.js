2bc4ee00a1966d76a19124daea2ca2bae6002b5d
Prince-jod <baghelprince164@gmail.com>
2026-08-06 03:19:49 +0530

    Auto commit

--- Attendencehai/server.js
+++ Attendencehai/server.js
@@ -1,23 +1,31 @@
-const express =require('express');
-const cors=require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
 
-const sequelize=require('./config/db');
+const sequelize = require("./config/db");
+require("./models");
+const authRoutes = require("./routes/auth.routes");
 
-const app=express();
-const path=require('path');
+const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-
 app.use(express.static(path.join(__dirname, "public")));
-sequelize.authenticate()
-.then(()=>console.log("database connected  successfully"))
-.catch((err)=>console.log("got an error",err));
 
-const port=3000;
+app.use("/api/auth", authRoutes);
 
-app.listen(port,()=>{
-  console.log("server is running");
-})
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database connected and models synced.");
+  })
+  .catch((err) => {
+    console.log("Database Error:", err);
+  });
 
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
