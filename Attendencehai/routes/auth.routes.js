e75328ac35392a56f3a99553c18509479585efd9
Prince-jod <baghelprince164@gmail.com>
2026-08-06 01:55:44 +0530

    Auto commit

---
+++ Attendencehai/routes/auth.routes.js
@@ -0,0 +1,9 @@
+//creating route for the register 
+
+const express =require('express');
+
+const router=express.Router();
+
+const {register}=require('../controllers/auth.controller');
+
+router.post()
