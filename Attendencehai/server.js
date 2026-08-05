0aa29d71a7e4a8f2bdfc742a033d1bad41cebca0
Prince-jod <baghelprince164@gmail.com>
2026-08-05 17:46:20 +0530

    Auto commit

--- Attendencehai/server.js
+++ Attendencehai/server.js
@@ -10,4 +10,12 @@ app.use(cors());
 app.use(express.json());
 
 sequelize.authenticate()
+.then(()=>console.log("database connected  successfully"))
+.catch((err)=>console.log("got an error",err));
+
+const port=3000;
+
+app.listen(port,()=>{
+  console.log("server is running");
+})
 
