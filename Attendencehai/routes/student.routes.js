b26d121b1ae091a2591a2009e6de2a07907f9ff6
Prince-jod <baghelprince164@gmail.com>
2026-08-07 00:19:48 +0530

    Auto commit

--- Attendencehai/routes/student.routes.js
+++ Attendencehai/routes/student.routes.js
@@ -2,9 +2,10 @@ const express =require('express');
 
 const router=express.Router();
 
-const {createStudent,getAllStudents}=require('../controllers/student.controller')
+const {createStudent,getAllStudents,getStudentById}=require('../controllers/student.controller')
 
 router.post('/',createStudent);
 router.get('/',getAllStudents);
+router.get('/:id',getStudentById);
 
 module.exports=router;
