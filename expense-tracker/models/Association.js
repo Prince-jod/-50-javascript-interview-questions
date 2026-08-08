da397ad5f4b0cdd731b2e18688c58eac97db5b66
Prince-jod <baghelprince164@gmail.com>
2026-08-08 14:27:30 +0530

    Auto commit

--- expense-tracker/models/Association.js
+++ expense-tracker/models/Association.js
@@ -1,12 +1,19 @@
 const User = require('./User');
 const Expense = require('./Expense');
-
+const Order = require("./Order");
 // user <-----> expense
 User.hasMany(Expense, {
   foreignKey: "userId",
 });
 
 Expense.belongsTo(User, {
+  foreignKey: "userId",
+});
+User.hasMany(Order, {
+  foreignKey: "userId",
+});
+
+Order.belongsTo(User, {
   foreignKey: "userId",
 });
 
