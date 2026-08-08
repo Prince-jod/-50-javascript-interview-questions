71b4ac881cc7f02e21fb423000d0997003530e1b
Prince-jod <baghelprince164@gmail.com>
2026-08-08 14:33:16 +0530

    Auto commit

---
+++ expense-tracker/routes/payment.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware = require("../middlewares/auth.middleware");
+const { createOrder } = require("../controllers/payment.controller");
+
+router.post("/create-order", authMiddleware, createOrder);
+
+module.exports = router;
