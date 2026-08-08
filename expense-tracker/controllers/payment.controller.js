7092466106c303175fbf55cb859359fc82006404
Prince-jod <baghelprince164@gmail.com>
2026-08-08 15:05:21 +0530

    Auto commit

--- expense-tracker/controllers/payment.controller.js
+++ expense-tracker/controllers/payment.controller.js
@@ -16,20 +16,20 @@ const createOrder = async (req, res) => {
 
     // 2. Create the order on Cashfree
     const request = {
-      order_amount: amount,
-      order_currency: "INR",
-      order_id: order.orderId,
+  order_amount: amount,
+  order_currency: "INR",
+  order_id: order.orderId,
 
-      customer_details: {
-        customer_id: String(userId),
-        customer_phone: req.user.phone || "9999999999",
-      },
+  customer_details: {
+    customer_id: String(userId),
+    customer_phone: "9999999999",
+  },
 
-      order_meta: {
-        return_url:
-          `http://localhost:3000/payment-success.html?order_id={order_id}`,
-      },
-    };
+  order_meta: {
+    return_url:
+      "http://localhost:3000/payment-success.html?order_id={order_id}",
+  },
+};
 
     const response = await cashfree.PGCreateOrder(request);
 
