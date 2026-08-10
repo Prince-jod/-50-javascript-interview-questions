c0f51ea9dd7c313e29a485fb9b761e87b0eb834b
Prince-jod <baghelprince164@gmail.com>
2026-08-10 16:06:24 +0530

    Auto commit

--- expense-tracker/public/js/dashboard.js
+++ expense-tracker/public/js/dashboard.js
@@ -602,7 +602,7 @@ if (leaderboardBtn) {
                             document.createElement("td");
 
                         nameCell.textContent =
-                            item.name;
+                            item.User.name;
 
 
                         // Total expense
