1945d4acb6903e5d66c3e537de51bdf5469b506d
Prince-jod <baghelprince164@gmail.com>
2026-08-11 00:56:41 +0530

    Auto commit

--- final_project_attendence/server.js
+++ final_project_attendence/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.get('/',(req,res)=>{
   res.sendFile(__dirname + "/view/index.html");
 });
-app.use('/api/attendace',attendanceRoutes);
+app.use('/api/attendance',attendanceRoutes);
 
 sequelize.authenticate()
 .then(()=>{
